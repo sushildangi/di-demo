c90f4bc3fc84e2e06bcd952468d76f41e1a48cdf
Sushil Dangi <dangi.sushil5@gmail.com>
2018-10-19 22:23:16 +0530

    Profile Annotation introduced

--- src/main/java/org/pyarts/didemo/service/PrimaryHindiGreetingService.java
+++ src/main/java/org/pyarts/didemo/service/PrimaryHindiGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("hi")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryHindiGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello - I am primary Greeting Service";
+        return "हैलो - मैं प्राथमिक हिंदी नमस्कार सेवा हूं।";
     }
 }
